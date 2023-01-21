2f8c99cb2cf6ecde0c0f50a103e317e48560baef
infotovignesh22 <122019744+infotovignesh22@users.noreply.github.com>
2023-01-21 22:53:11 +0530

    Adding some names

--- Simple.java
+++ Simple.java
@@ -1,5 +1,9 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Vicky");  
+        System.out.println("Hello Dhana");
+        System.out.println("Hello Arthi");
+        System.out.println("Hello Renu");
+        System.out.println("Hello Mohan");
     }  
 }  
